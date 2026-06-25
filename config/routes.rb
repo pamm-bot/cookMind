aecf556e1eb63d1ddaecded38c1adc7aa0086ed5
Lilian Sejean <sejeanlilian@gmail.com>
2026-06-25 23:55:25 +0200

    created chats methods and AI forms on chats view, add also a link_to on ingredients index view

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,14 @@ Rails.application.routes.draw do
 
   resources :profils, only: [:show, :edit, :update]
   resources :recipes, only: [:index, :show]
-  resources :chats, only: [:index, :show, :create]
-  resources :messages, only: [:create]
-  resources :ingredients, only: [:index, :create]
+
+  resources :ingredients, only: [:index, :create] do
+    resources :chats, only: [:show, :create]
+  end
+
+  resources :chats, only: [:show, :create] do
+    resources :messages, only: [:create]
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
