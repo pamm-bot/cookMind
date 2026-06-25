e306e404c189fa198b4df6a1dbd3a1f4af228698
farzana abul kalam azad <bfarzane@hotmail.com>
2026-06-25 15:46:38 +0200

    Add recipes index and show views

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   resources :profils, only: [:show, :edit, :update]
-  resources :recipes, only: [:index, :show]
+  resources :recipes, only: [:index, :show, :create, :destroy]
   resources :chats, only: [:index, :show, :create]
   resources :messages, only: [:create]
   resources :ingredients, only: [:index, :create]
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
+
