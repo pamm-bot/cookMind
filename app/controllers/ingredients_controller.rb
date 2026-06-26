5dfac39213b60f3402355e68144721c49c044135
Pamela Morganti <pahelemoba@gmail.com>
2026-06-26 21:50:32 +0200

    Add Ingredients controller and routes setup

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,7 +1,18 @@
 class IngredientsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_profil
   def index
   end
 
   def create
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_profil
+    @profil = current_user.profil || current_user.create_profil
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   resources :profils, only: [:show, :edit, :update]
-  resources :recipes, only: [:index, :show]
+  resources :recipes, only: [:index, :show, :destroy]
   resources :chats, only: [:index, :show, :create]
   resources :messages, only: [:create]
-  resources :ingredients, only: [:index, :create]
+  resources :ingredients, only: [:index, :create, :destroy]
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
