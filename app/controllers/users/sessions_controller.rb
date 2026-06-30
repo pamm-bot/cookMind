d5dbaafe42ba20c6ce8cc97f4fac4366709e657c
Pamela Morganti <pahelemoba@gmail.com>
2026-06-30 22:37:18 +0200

    fix application_controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,27 +1,21 @@
-# frozen_string_literal: true
-
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :redirect_if_authenticated, only: [:new]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  private
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def redirect_if_authenticated
+    if user_signed_in?
+      redirect_to root_path, alert: "Sei già loggata!"
+    end
+  end
+endclass Users::SessionsController < Devise::SessionsController
+  before_action :redirect_if_authenticated, only: [:new]
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  private
 
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def redirect_if_authenticated
+    if user_signed_in?
+      redirect_to root_path, alert: "Sei già loggata!"
+    end
+  end
 end
