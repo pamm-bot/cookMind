f6b6ed91ceadc58feb1b73aa6f2e340f869b1d0c
Lilian Sejean <sejeanlilian@gmail.com>
2026-06-30 20:49:44 +0200

    start model message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,24 @@
 class MessagesController < ApplicationController
   def create
+    @chat = current_user.chats.find(params[:chat_id])
+    # @ingredients = chat.ingredients
+    @message = Message.new(message_params)
+    @message.chat = @chat
+    @message.role = "user"
+
+    if @message.save
+      ruby_llm_chat = RubyLLM.chat
+      response = ruby_llm_chat.with_instructions(SYSTEM_PROMPT).ask(@message.content)
+      Message.create(role: "a chef", content: response.content, chat: @chat)
+      redirect_to chat_path(@chat)
+    else
+      render "/ingredients", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
   end
 end
